fa06546507452a2c8223d8dc1b17549a3ec5246f
Hammad Ali <i220628@nu.edu.pk>
2025-07-23 20:40:34 +0500

    added media queries

--- Frontend/src/Navigation/Navigation.jsx
+++ Frontend/src/Navigation/Navigation.jsx
@@ -9,15 +9,14 @@ const Navigation = () => {
   return (
     <nav className={`navbar ${menuOpen ? 'active' : ''}`}>
       {/* Logo */}
-     <div className="logo">
-     <Link to="/Home" className="logo-container">
-       <img src="Company Logo.webp" alt="CraftHive Solutions Logo" className="logo-img" />
-       <div className="logo-text">
-         CraftHive <br /> Solution
-       </div>
-     </Link>
-   </div>
-   
+      <div className="logo">
+        <Link to="/Home" className="logo-container">
+          <img src="Company Logo.webp" alt="CraftHive Solutions Logo" className="logo-img" />
+          <div className="logo-text">
+            CraftHive <br /> Solution
+          </div>
+        </Link>
+      </div>
 
       {/* Desktop and Mobile Menu */}
       <ul className={`desktop-menu ${menuOpen ? 'mobile-menu' : ''}`}>
@@ -29,15 +28,16 @@ const Navigation = () => {
             <li><Link to="/HighlightingServices"> Highlighting Services</Link></li>
           </ul>
         </li>
-        <li><Link to="#">Services</Link>
+        <li>
+          <Link to="#">Services</Link>
           <ul className="dropdown">
             <li><Link to="/AIServices"> AI Automation </Link></li>
             <li><Link to="/WebDevelopment"> Web Development</Link></li>
           </ul>
         </li>
-        <li><Link to="/portfolio">Projects</Link></li>
-        <li><Link to="/About">About Us</Link></li>
-        <li><Link to="/ContactForm" className='connect-btn'>Contact Us </Link></li>
+        <li><Link to="/portfolio">Projects</Link></li> {/* Direct link */}
+        <li><Link to="/About">About Us</Link></li>   {/* Direct link */}
+        <li><Link to="/ContactForm" className='connect-btn'>Contact Us</Link></li> {/* Direct link */}
       </ul>
 
       {/* Hamburger Icon */}
